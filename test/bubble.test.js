076e33fee47b1a0e074c391222d73b246b0b3cfe
rbtprograms <rbt61690@gmail.com>
2018-07-17 21:21:34 -0700

    returns 0 on empty array

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -3,10 +3,23 @@ const bubbleSorter = require('../lib/bubble');
 
 describe('bubble sorting algorithm', () => {
 
+    it('returns 0 when given an empty array', () => {
+        const empty = [];
+        const emptySorted = bubbleSorter(empty);
+        assert.equal(emptySorted, 0);
+    });
+    
     it('can put an array of two in order', () => {
         const small = [9, 1];
         const smallSorted = bubbleSorter(small);
         assert.deepEqual(smallSorted, [1, 9]);
+    });
+    
+    it('can put an array of 5 in order', () => {
+        const medium = [8, 4, 15, 1, 3];
+        const mediumSorted = bubbleSorter(medium);
+        console.log(mediumSorted);
+        assert.deepEqual(mediumSorted, [1, 3, 4, 8, 15]);
     });
 
 });
