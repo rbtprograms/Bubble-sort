9b74afd43480d0dd41560c1e1e3f71edae9fe010
rbtprograms <rbt61690@gmail.com>
2018-07-17 22:31:38 -0700

    final push

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -21,7 +21,7 @@ describe('bubble sorting algorithm', () => {
         assert.deepEqual(mediumSorted, [1, 3, 4, 8, 15]);
     });
     
-    it('can put an array of 20 in order', () => {
+    it.only('can put an array of 20 in order', () => {
         const large = [5, 9, 20, 19, 1, 3, 4, 18, 17, 6, 8, 7, 2, 11, 13, 10, 12, 14, 15, 16];
         const largeSorted = bubbleSorter(large);
         assert.deepEqual(largeSorted, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
