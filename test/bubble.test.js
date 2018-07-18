bab45657dc05fda8b4ff8fe418165fa64cb8219b
rbtprograms <rbt61690@gmail.com>
2018-07-17 21:47:48 -0700

    can sort an array of 5

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -18,8 +18,14 @@ describe('bubble sorting algorithm', () => {
     it('can put an array of 5 in order', () => {
         const medium = [8, 4, 15, 1, 3];
         const mediumSorted = bubbleSorter(medium);
-        console.log(mediumSorted);
         assert.deepEqual(mediumSorted, [1, 3, 4, 8, 15]);
     });
+    
+    it('can put an array of 20 in order', () => {
+        const large = [5, 9, 20, 19, 1, 3, 4, 18, 17, 6, 8, 7, 13, 10, 12, 14, 15, 16];
+        const largeSorted = bubbleSorter(large);
+        console.log(largeSorted);
+        assert.deepEqual(largeSorted, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    })
 
 });
